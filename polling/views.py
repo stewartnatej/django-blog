63c6fbfa839a98cc553f2b3d549386ad738d09c0
Nate Stewart <njaspers@pm.me>
2023-05-23 20:36:33 -0700

    Add class-based views for polling

--- polling/views.py
+++ polling/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from polling.models import Poll
 from asgiref.sync import sync_to_async
 
 
 def list_view(request):
+    """function version of the view"""
     context = {'polls': Poll.objects.all()}
     return render(request, 'polling/list.html', context)
+
+
+class PollListView(ListView):
+    """more robust class-based view"""
+    model = Poll
+    template_name = 'polling/list.html'
 
 
 async def list_view_async(request):
@@ -22,6 +32,7 @@ async def list_view_async(request):
 
 
 def detail_view(request, poll_id):
+    """function version of the view"""
     try:
         poll = Poll.objects.get(pk=poll_id)
     except Poll.DoesNotExist:
@@ -36,3 +47,21 @@ def detail_view(request, poll_id):
 
     context = {'poll': poll}
     return render(request, 'polling/detail.html', context)
+
+
+class PollDetailView(DetailView):
+    """more robust class-based view"""
+    model = Poll
+    template_name = 'polling/detail.html'
+
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+
+        if request.POST.get("vote") == "Yes":
+            poll.score += 1
+        else:
+            poll.score -= 1
+        poll.save()
+
+        context = {"poll": poll}
+        return render(request, self.template_name, context)
