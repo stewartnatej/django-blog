57868eee60525f47d0672ffe5d92fa49238eaf3d
Nate Stewart <njaspers@pm.me>
2023-05-27 18:37:15 -0700

    Change Heroku static file setting

--- frog_jog_blog/heroku.py
+++ frog_jog_blog/heroku.py
@@ -1,20 +1,23 @@
 """
 Special settings that will only be used when deploying to Heroku
 """
-import os
+from os import path, environ
 import dj_database_url
 from .settings import *
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="sqlite:///" + os.path.join(BASE_DIR, "../db.sqlite3")
+        default="sqlite:///" + path.join(BASE_DIR, "../db.sqlite3")
     )
 }
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-STATIC_ROOT = STATIC_ROOT = "/app/static/"
-SECRET_KEY = os.environ.get("django_key")
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, 'static'),
+]
+SECRET_KEY = environ.get("django_key")
 ALLOWED_HOSTS = ["*"]
 
 MIDDLEWARE = (
